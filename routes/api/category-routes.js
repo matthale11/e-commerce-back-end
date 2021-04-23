a0ad55e4ab869b78dc4baf7312426e1d3c7af84a
Matt Hale <mhale@factharbor.com>
2021-04-23 15:13:49 -0400

    corrected put routes for category and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,12 +47,11 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update(
-      { category_name: req.body.category_name },
-      { where: req.params.id }
-    );
+    const categoryData = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
     if (!categoryData) {
-      res.status(404).json({ message: "No category with that id!"});
+      res.status(404).json({ message: "No category with that id!" });
     }
     res.status(200).json(categoryData);
   } catch (err) {
